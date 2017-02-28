a8f831bcb0d9d2dcf783552513404097260aca1b
stevenjmesser <stevenjmesser@gmail.com>
2017-02-28 22:00:25 +0000

    Scroll time across display and rotate readout.

--- sp-clock.py
+++ sp-clock.py
@@ -2,12 +2,15 @@ import sys
 import time
 import scrollphat
 
-current = time.strftime("%H:%M")
+scrollphat.set_brightness(50)
+scrollphat.set_rotate(True)
+current = time.strftime("  "+"%H:%M"+" ")
 
 while True:
    try:
+        scrollphat.scroll()
         scrollphat.write_string(current)
-        time.sleep(1)
+        time.sleep(0.35)
    except KeyboardInterrupt:
         scrollphat.clear()
         sys.exit(-1)
