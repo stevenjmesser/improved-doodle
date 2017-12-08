4ebd8e4e2f8a996e9d28cb56897138fe45e5591f
stevenjmesser <stevenjmesser@gmail.com>
2017-12-08 20:12:38 +0000

    Commenting

--- sp-clock.py
+++ sp-clock.py
@@ -8,30 +8,30 @@ scrollphat.clear()
 scrollphat.set_brightness(7)
 scrollphat.set_rotate(True)
 
-def scroll_message(message):
-    scrollphat.write_string(message, 11)
-    for x in range(scrollphat.buffer_len()):
-        scrollphat.scroll()
-        time.sleep(0.1)
-        pass
-    scrollphat.clear()
+#def scroll_message(message):
+#    scrollphat.write_string(message, 11)
+#    for x in range(scrollphat.buffer_len()):
+#        scrollphat.scroll()
+#        time.sleep(0.1)
+#        pass
+#    scrollphat.clear()
 
-scroll_message("SET ALARM")
-scroll_message("E.G. 07:00 FOR 7 A.M.")
-scrollphat.write_string("HRS")
+#scroll_message("SET ALARM")
+#scroll_message("E.G. 07:00 FOR 7 A.M.")
+#scrollphat.write_string("HRS")
 
-hours = input()
+#hours = input()
 
-scrollphat.clear()
-scroll_message("YOU SAID " + hours + " HOURS")
-scrollphat.write_string("MS")
+#scrollphat.clear()
+#scroll_message("YOU SAID " + hours + " HOURS")
+#scrollphat.write_string("MS")
 
-minutes = input()
+#minutes = input()
 
-scrollphat.clear()
-scroll_message("YOU SAID " + minutes + " MINUTES")
+#scrollphat.clear()
+#scroll_message("YOU SAID " + minutes + " MINUTES")
 
-scroll_message("I'LL WAKE YOU AT " + hours + ":" + minutes)
+#scroll_message("I'LL WAKE YOU AT " + hours + ":" + minutes)
 
 while True:
     scrollphat.write_string(time.strftime("%H:%M"), 11)
